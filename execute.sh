4206de82c398c44a118f2481c5efb929e67528de
Berndinox <berndinox@gmail.com>
2020-07-25 14:09:38 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "Query parameter: url=$url"
+echo "Processing: $url"
+mkdir /var/opt/webhookd/work/$url
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains $url --no-parent https://$url -P /var/opt/webhookd/work/$url
